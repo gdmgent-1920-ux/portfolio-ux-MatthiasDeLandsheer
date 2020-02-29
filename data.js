340319941d92f5fa8f1e0820996bf822bcf520ba
MatthiasDeLandsheer <61147789+MatthiasDeLandsheer@users.noreply.github.com>
2020-02-29 14:49:46 +0100

    Share link app Matthias De Landsheer

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/f5a4621e-26df-4811-5007-f39c18487c52-8395/",
                 "target": "_blank"
             }
         ]
